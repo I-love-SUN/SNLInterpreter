acebe31f9bef313d33b92d198d0fb8155803ae30
I-love-SUN <814172489@qq.com>
2023-05-09 16:15:22 +0800

    1

---
+++ cmake-build-debug/CMakeFiles/SNLInterpreter.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SNLInterpreter.dir/main.cpp.obj"
+  "SNLInterpreter.exe"
+  "SNLInterpreter.exe.manifest"
+  "SNLInterpreter.pdb"
+  "libSNLInterpreter.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SNLInterpreter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/SNLInterpreter.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SNLInterpreter.
+# This may be replaced when dependencies are built.
