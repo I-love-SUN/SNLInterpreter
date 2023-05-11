001bf2c2633df0f47db0ceb6ffa0faab2e27f843
zqxKarry <2908874296@qq.com>
2023-05-12 00:59:09 +0800

    scanner.cpp即将完善

--- global.h
+++ global.h
@@ -48,7 +48,7 @@ typedef enum {
     ENDFILE,    ERROR,
 
     /*保留字*/
-    PROGRAM,    PROCEDURE,      TYPE,   VAR,    TF,
+    PROGRAM,    PROCEDURE,      TYPE,   VAR,    IF,
     THEN,       ELSE,           FI,     WHILE,  DO,
     ENDWH,      BEGIN,          END,    WRITE,  READ,
     ARRAY,      OF,             RECORD, RETURN,
@@ -60,7 +60,7 @@ typedef enum {
     ID,     INTC,       CHARC,
 
     /*特殊符号：运算符等等*/
-    ASSIGN,     EQ,     LI,         PLUS,       MINUS,
+    ASSIGN,     EQ,     LT,         PLUS,       MINUS,
     TIMES,      OVER,   LPAREN,     RPAREN,     DOT,
     COLON,      SEMI,   COMMA,      LMIDPAREN,  RMIDPAREN,
     UNDERANGE,
@@ -77,6 +77,53 @@ typedef struct tokenType
     LexType Lex;        //记录该单词的词法信息
     char    Sem[MAXTOKENLEN+1]; //记录该单词的语义信息
 }TokenType;
+
+/*toke序列链表*/
+typedef struct node
+{
+    TokenType  Token;/*单词*/
+    struct node *nextToken;
+}ChainNodeType;
+
+
+/*
+ * 全局共享
+ */
+/*源代码文本文件指针*/
+extern FILE *source;
+
+/*列表输出指针*/
+extern FILE *listing;
+
+/*词法分析结果tokenlist存储文件指针*/
+extern FILE *Fp;
+
+/*源文件的行号*/
+extern int lineno;
+
+/*Token序列中单词总数,初始为0*/
+extern int Tokennum;
+
+
+
+
+
+/*
+ * 追踪标志：本次指定的输出设备是文件listing
+ */
+/*源代码跟踪标志判断是否要将源程序列表输出到指定设备*/
+extern int EchoSource;
+/*词法分析追踪标志，初始为FALSE。为TRUE,将词法分析信息输出到指定设备*/
+extern int TraceScan;
+/*语法分析追踪标志，初始为FALSE,为TRUE，将语法分析信息输出到指定设备*/
+extern int TraceParse;
+/*符号表追踪标志，初始为TRUE,为TRUE，将语义分析产生的符号表信息输出到指定设备*/
+extern int TraceTable;
+/*目标代码追踪*/
+extern int TraceCode;
+/*错误追踪标志，防止错误进一步传递*/
+extern int Error;
+
 
 
 
